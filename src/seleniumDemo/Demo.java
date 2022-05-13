f2340ab14e6c302ba26864ddae105e752ac316e9
user <user@LAPTOP-AP9GR7OC>
2022-05-13 16:59:04 +0530

    adsdg

--- src/seleniumDemo/Demo.java
+++ src/seleniumDemo/Demo.java
@@ -21,6 +21,8 @@ public class Demo {
 	public static void main(String[] args)
 			throws InterruptedException, HeadlessException, UnsupportedFlavorException, IOException {
 		// TODO Auto-generated method stub
+		int n = 1;
+		int b=2;
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\user\\eclipse-workspace\\seleniumDemo\\Drivers\\chromedriver.exe");
 		WebDriver driver;
@@ -43,7 +45,7 @@ public class Demo {
 		// Clicking on Continue button
 		driver.findElement(By.className("_1wy2dtkm")).click();
 		Thread.sleep(1000);
-		for (int k = 0; k <= 3; k++) {
+		for (int k = 0; k <= n; k++) {
 			// Clicking on new account tab
 			driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div[3]/div[1]/div[2]/div[1]/div[2]/div/div[2]"))
 					.click();
@@ -65,35 +67,32 @@ public class Demo {
 			// Click on Check box and Continue
 			driver.findElement(By.className("_1pknp6j")).click();
 			driver.findElement(By.className("_t2mr9i")).click();
-			// Copy the words code(number)
-
-			System.out.println("Done");
 			Thread.sleep(2000);
 
 			// Copy all the index value displaying on the phrase compare page
 			for (int i = 1; i <= 4; i++) {
-
+				// getting index of mnemonic array
 				String s = driver.findElement(By.cssSelector("._1frr4gj>div:nth-child(" + i + ")>div>div:nth-child(2)"))
 						.getText();
-				int code = Integer.parseInt(s);
+				int code = Integer.parseInt(s);// converting string number into int
 				// Store the word in t variable.
 				String t = text1[code - 1];
 				// Fetch all the words displaying on the phrase comparing page
 				for (int j = 1; j <= 4; j++) {
+					// checking text in first option
 					String g = driver.findElement(By.cssSelector("._zmhbi1>div:nth-child(" + j + ")>button")).getText();
-					System.out.println("dvalue assign to :" + g + j + "times");
 
-					// Compare the word according to the index value
-
+					// Comparing t with g
 					if (t.equals(g)) {
-						System.out.println("reached");
+						//System.out.println("reached");
 						driver.findElement(By.cssSelector("._zmhbi1>div:nth-child(" + j + ")>button")).click();
 						break;
 					}
 				}
 			}
 			// Enter the account name
-			driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div[3]/div[1]/div[5]/div/input")).sendKeys("New");
+			driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div[3]/div[1]/div[5]/div/input"))
+					.sendKeys("New" + k + "");
 			Thread.sleep(2000);
 			// Click on the create account
 			driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div[3]/div[1]/div[6]/button")).click();
@@ -106,12 +105,56 @@ public class Demo {
 			driver.findElement(
 					By.xpath("/html/body/div[2]/div/div[1]/div/div/div[2]/div[1]/div/div/div[2]/div/button/div/div"))
 					.click();
-			System.out.println("Completed");
 			Thread.sleep(3000);
+			// Click on add new account
 			driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div[1]/div/div[2]/div[1]/div[1]/span[1]/span"))
 					.click();
 			Thread.sleep(3000);
-			// }
 		}
+		
+		
+		
+		
+		
+		
+		// Navigate to qa-stage.ultradedev.net
+		driver.navigate().to("https://qa-stage.ultradedev.net/");
+		// Click on the connect button
+		driver.findElement(By.className("connectWallet_head")).click();
+		// Handle multiple windows
+		String firstwindow = driver.getWindowHandle();
+		Set<String> windows = driver.getWindowHandles();
+		Iterator<String> itr = windows.iterator();
+		while (itr.hasNext()) {
+			String window = itr.next();
+			driver.switchTo().window(window);
+			// Navigate to next window
+			if (driver.getTitle().equals("MyAlgo Connect - Allow")) {
+				// Enter the password
+				driver.findElement(By.xpath(
+						"//*[@id=\"root\"]/div/div/div/div/div[1]/div[3]/div/div[2]/div/form/div[1]/div[1]/input"))
+						.sendKeys("Anu@2022");
+				Thread.sleep(2000);
+				driver.findElement(By
+						.xpath("//*[@id=\"root\"]/div/div/div/div/div[1]/div[3]/div/div[2]/div/form/div[2]/button[2]"))
+						.click();
+				Thread.sleep(2000);
+				//Select all the created wallets
+				for(int l=2;l<=b+1;l++) {
+				driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div/div/div[1]/div[2]/div["+ l +"]")).click();
+				}
+				Thread.sleep(2000);
+				//Click on the continue button
+				driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div/div/div[1]/div[3]/div/div/button[2]"))
+						.click();
+				
+			
+
+		}
+			
+
 	}
-}
+		//Switch to parrent tab 
+		driver.switchTo().window(firstwindow);
+		driver.findElement(By.xpath("//*[@id=\"basic-navbar-nav\"]/div/a[1]")).click();
+	}}
